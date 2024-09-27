3044b8c9aa1105df6d480ae576e2d73fb4cc7ade
Aman Jaiswal <89749348+Amanj-18@users.noreply.github.com>
2024-09-28 01:31:18 +0530

    commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+// created a server
 // HMR Hot Module Replacement
-// File Watcher algorithm
+// File Watcher algorithm - C++
 // BUNDLING
 // MINIFY
 // Cleaning our Code
-// Dev abd Production Build
-// Super Fast build algorithe
+// Dev and Production Build
+// Super Fast build algorithm
 // Image Optimization
 // Caching while development
 // Compression
@@ -13,12 +14,15 @@
 // port Number
 // Consistent Hashing Algorithm
 // Zero Config
+// tree shaking - removing unwanted
+// transitive dependencies
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const head1=React.createElement("h1",{id:"title"},"heading 1"); 
-const head2=React.createElement("h2",{id:"title"},"heading 2");
+const head1=React.createElement("h1",{id:"title",key:"h1"},"heading 1"); 
+const head2=React.createElement("h2",{id:"title",key:"h2"},"heading 2");
+console.log(head1);
 const con=React.createElement("div",{id:"container"},[head1,head2]);
 const root2 = ReactDOM.createRoot(document.getElementById('root'));
 root2.render(con);
